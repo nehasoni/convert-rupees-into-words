ff0c96522961779171637bad38f30604a51f4dcd
Neha Soni <neha@bizongo.in>
2018-09-30 18:43:03 +0530

    added on new test case

--- test/test.js
+++ test/test.js
@@ -14,8 +14,13 @@ describe('#indianCurrencyToWordsConverter', function() {
     expect(result).to.equal('Twelve Thousand Six Hundred Seventy Seven');
   });
 
-  it('should convert Lakhs', function() {
+  it('should convert lakhs', function() {
     var result = index.indianCurrencyToWordsConverter(567123);
     expect(result).to.equal('Five Lakh Sixty Seven Thousand One Hundred Twenty Three');
+  });
+
+  it('should convert crores', function() {
+    var result = index.indianCurrencyToWordsConverter(160567123);
+    expect(result).to.equal('Sixteen Crore Five Lakh Sixty Seven Thousand One Hundred Twenty Three');
   });
 });
